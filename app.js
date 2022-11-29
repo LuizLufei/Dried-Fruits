d8b20a2e0c34f87ea2c36c52e95e03cf4cdb157f
luizLufei <lazizbek21207@mail.ru>
2022-11-29 22:41:56 +0500

    test 1

---
+++ app.js
@@ -0,0 +1,82 @@
+
+funcsion(){
+  $(".eng").click(function() {
+
+  })
+}
+
+
+
+
+
+
+
+
+// 22222222
+// const langE = document.querySelector('.main_up_right1');
+// const link = document.querySelectorAll('div');
+// const langE1 =document.querySelector('.eng');
+// const langE2 = document.querySelector(".eng");
+
+// link.forEach(el =>{
+//   el.addEventListener('click', () =>{
+//     langE.querySelector('active').classList.remove('active');
+//       el.classList.add('active');
+
+//       const attr = el.getAttribute('language');
+//       langE1.textContent = data[attr].eng
+//   });
+// });
+
+// var data = {
+//   "eng":{
+//     "button_request":"Request the price",
+
+//   },
+//   "ru":{
+//     "button_request":"Получит прайс",
+//   },
+// }
+
+
+
+
+
+
+
+
+
+
+
+// 11111
+// let button = document.querySelector(".button"),
+//     link = document.querySelectorAll("a"),
+//     button1 = document.querySelector(".button_request"),
+
+
+
+// link.forEach(el=>{
+//      el.addEventListener("click", ()=>{
+//           button.querySelector(".active").classList.remove("active"),
+//           el.classList.add("active");
+//      });
+
+//      let attr = el.getAttribute("button")
+  
+//      button_request.textContent = data[attr].title
+     
+// })
+
+
+// const language = {
+//   eng: {
+//     //    main_nav_item: "",
+//     //    eco:"",
+//     //    eco2:"",
+//     //    main_up_left_down:"",
+//     button_request: "Request a call",
+//   },
+//   ru: {
+//     button_request: "Получит прайс",
+//   },
+// };
